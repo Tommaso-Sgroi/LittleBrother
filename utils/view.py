fc58c58ca0a811ad2defd65574cdc15c9bdc89b2
Tommaso-Sgroi <sgroi_tom@yahoo.com>
2025-01-24 11:59:14 +0100

    Local import in motion_detector.py Add optional parameter to view utility function

--- utils/view.py
+++ utils/view.py
@@ -1,15 +1,16 @@
 import cv2 as cv
 
 
-def view(frame, *, scale=0.5):
+def view(frame, *, scale=0.5, winname='Frame'):
     """
     :param frame: frame to draw on
     :param scale: scale factor to scale the frame by
     :return: stop the drawing of the frame
+    :winname: name of the opencv window, use different names to create multiple windows
     """
     # Resize frame to    a normal view
     frame = cv.resize(frame, None, fx=scale, fy=scale, interpolation=cv.INTER_LINEAR)
-    cv.imshow('Frame', frame)
+    cv.imshow(winname, frame)
     key = cv.waitKey(1)
     if key in [27, ord('q'), ord('Q')]:
         return False
