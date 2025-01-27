490ff47e2a6c7184c26352ffddd359f791038a63
Tommaso-Sgroi <sgroi_tom@yahoo.com>
2025-01-27 18:53:46 +0100

    Add people detector in camera_env_demo.py

--- demos/camera_env_demo.py
+++ demos/camera_env_demo.py
@@ -1,4 +1,7 @@
 from camera.video_frame_initializer import initializer
+from face_recognizer.face_recognizer import FaceRecognizer
+from motion_detector.motion_detector import MotionDetector
+from people_detector.people_detector import PeopleDetector
 from utils.view import view
 from utils.logger import init_logger
 import logging
@@ -14,6 +17,24 @@ if __name__ == '__main__':
     videos = [video.strip() for video in videos]
 
     controller = initializer(videos, timeout=-1, max_queue_size=100)
+
+    yolosize = 'n'
+    yolo11 = PeopleDetector(f"yolo11{yolosize}.pt", verbose=False, )
+    yolo11.to('cpu')
+
+    overlap_threshold = 0.0005
+    area_threshold = 700
+    motion_detector = MotionDetector(area_threshold=area_threshold, overlap_threshold=overlap_threshold)
+
+    face_recognizer = FaceRecognizer(threshold=0.5)
+    # face_5 = Image.open('demo_images/face_5.jpg')
+    # face_recognizer.enroll_face(face_5, 'Michael Scott', overwrite=True)
+
+    # probs, bboxes, result = yolo11.detect(frame)
+    # print('confidence scores', probs)
+    # print('bboxes', bboxes)
+    # annotated_frame = result.plot()
+
     # controller = initializer(['WiseNET/wisenet_dataset/video_sets/set_1/video1_1.avi'])
 
     # test video source
@@ -30,12 +51,17 @@ if __name__ == '__main__':
     controller.start()
     from time import sleep
 
+
     i = 0
     while True:
         sleep(1)
         frames = controller.get_frames()
         for video_id, frame in frames:
-            view(frame, winname=video_id)
+            probs, bboxes, result = yolo11.detect(frame)
+            print('confidence scores', probs)
+            print('bboxes', bboxes)
+            annotated_frame = result.plot()
+            view(annotated_frame, winname=video_id)
 
         if len(frames) == 0:
             break
