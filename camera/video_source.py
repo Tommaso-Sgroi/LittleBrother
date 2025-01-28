6962a6e9aa82c486743315d72913e0f61cdff7dc
Tommaso-Sgroi <sgroi_tom@yahoo.com>
2025-01-28 17:06:52 +0100

    Add video_source.py frame rate limit in initializer parameters

--- camera/video_source.py
+++ camera/video_source.py
@@ -1,21 +1,58 @@
 import queue
-
-from .frame_source import FrameSource
+import time
+import functools
 import cv2 as cv
 from multiprocessing import Queue
-from .utils import rate_limit
+from .frame_source import FrameSource
+
+
+def rate_limit(method):
+    """
+    Decoratore che limita la frequenza di chiamata di 'method'
+    in base a 'self.fps', salvando l'ultimo timestamp in 'self._last_call'.
+    """
+
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        # Se l'istanza non ha _last_call, lo inizializziamo a 0
+        if not hasattr(self, '_last_call'):
+            self._last_call = 0
+
+        # Se non esiste 'self.fps', usiamo un default (ad es. 30)
+        fps = getattr(self, 'fps', 30)
+        if fps <= 0:
+            # Se per qualche ragione fps è 0 o negativo, nessun rate limit
+            return method(self, *args, **kwargs)
+
+        min_interval = 1.0 / fps
+        now = time.time()
+        elapsed = now - self._last_call
+
+        # Se abbiamo chiamato il metodo troppo di recente, aspettiamo
+        if elapsed < min_interval:
+            time.sleep(min_interval - elapsed)
+
+        result = method(self, *args, **kwargs)
+        self._last_call = time.time()
+        return result
+
+    return wrapper
+
 
 class VideoSource(FrameSource):
-
-    def __init__(self, id, video_path: str, fifo_queue: Queue, timeout=0.1):
-        self.video_path = video_path
-        # video_capture = cv.VideoCapture(video_path)
-        self.queue = fifo_queue
-        self.timeout = timeout
+    def __init__(self, id, video_path: str, fifo_queue: Queue, timeout=0.1, fps=30):
         super().__init__(id, stream=None, daemon=False)
 
-    @rate_limit(30)
+        self.video_path = video_path
+        self.queue = fifo_queue
+        self.timeout = timeout
+        self.fps = fps
+
+    @rate_limit
     def read(self):
+        """
+        Legge un frame dal video. L'accesso è rate-limitato dal decoratore.
+        """
         return self.stream.read()
 
     def next(self):
@@ -39,7 +76,7 @@ class VideoSource(FrameSource):
                 frame = self.next()
                 try:
                     self.queue.put([(self.id, frame)], timeout=self.timeout)
-                except queue.Full as qf:
+                except queue.Full:
                     self.logger.debug(f'cannot send video frame: queue full, skipping frame')
                 except Exception as ex:
                     self.logger.critical(f'cannot send video frame: %s', ex)
--- demos/camera_env_demo.py
+++ demos/camera_env_demo.py
@@ -2,8 +2,6 @@ from queue import Empty
 from time import sleep
 
 from camera.video_frame_initializer import initializer
-from face_recognizer.face_recognizer import FaceRecognizer
-from motion_detector.motion_detector import MotionDetector
 from people_detector.people_detector import PeopleDetector
 from utils.view import view
 from utils.logger import init_logger
@@ -18,12 +16,12 @@ if __name__ == '__main__':
         ../datasets/WiseNET/wisenet_dataset/video_sets/set_1/video1_5.avi""".split('\n')
 
     videos = [video.strip() for video in videos]
+    fps = 120
+    controller = initializer(videos, timeout=-1)
 
-    controller = initializer(videos, timeout=-1, max_queue_size=30*len(videos))
-
-    # yolosize = 'n'
-    # yolo11 = PeopleDetector(f"yolo11{yolosize}.pt", verbose=False, )
-    # yolo11.to('cpu')
+    yolosize = 'n'
+    yolo11 = PeopleDetector(f"yolo11{yolosize}.pt", verbose=False, )
+    yolo11.to('cpu')
     #
     # overlap_threshold = 0.0005
     # area_threshold = 700
@@ -33,20 +31,27 @@ if __name__ == '__main__':
 
 
     MULTI_THREAD = False
+    DETECT_PEOPLE = True
+    def plot_detected_people(sourceids_frames):
+        for video_id, frame in sourceids_frames:
+            probs, bboxes, result = yolo11.detect(frame)
+            print('confidence scores', probs)
+            print('bboxes', bboxes)
+            annotated_frame = result.plot()
+            view(annotated_frame, winname=video_id)
+
+
     if MULTI_THREAD:
         controller.start()
         i = 0
         while True:
             sleep(1)
             sourceids_frames = controller.get_frames()
-            # for video_id, frame in frames:
-            #     probs, bboxes, result = yolo11.detect(frame)
-            #     print('confidence scores', probs)
-            #     print('bboxes', bboxes)
-            #     annotated_frame = result.plot()
-            #     view(annotated_frame, winname=video_id)
-            for video_id, frame in sourceids_frames:
-                view(frame, winname=video_id)
+            if DETECT_PEOPLE:
+                plot_detected_people(sourceids_frames)
+            else:
+                for video_id, frame in sourceids_frames:
+                    view(frame, winname=video_id)
     else:
         controller.start_frame_sources()
         while controller.has_alive_sources():
@@ -54,9 +59,11 @@ if __name__ == '__main__':
                 sourceids_frames = controller.fetch_and_get_frames()
             except Empty:
                 continue
-            for sourceid, frame in sourceids_frames:
-                view(frame, winname=sourceid)
-
+            if DETECT_PEOPLE:
+                plot_detected_people(sourceids_frames)
+            else:
+                for video_id, frame in sourceids_frames:
+                    view(frame, winname=video_id)
 
     # face_5 = Image.open('demo_images/face_5.jpg')
     # face_recognizer.enroll_face(face_5, 'Michael Scott', overwrite=True)
