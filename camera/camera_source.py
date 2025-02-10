5c6f7104f844540b434c9095488f5bc49eb9f1fe
Tommaso-Sgroi <sgroi_tom@yahoo.com>
2025-02-10 17:33:28 +0100

    Add FakeCameraSource

--- camera/camera_source.py
+++ camera/camera_source.py
@@ -1,7 +1,7 @@
 from multiprocessing import Queue
 
 from camera.frame_source import QueuedFrameSource
-
+from cv2 import VideoCapture
 
 class CameraSource(QueuedFrameSource):
 
@@ -16,4 +16,18 @@ class CameraSource(QueuedFrameSource):
         super().queue_video_frame(frame)
 
 
+class FakeCameraSource(CameraSource):
+    """
+    Fakes a camera source by reading from a video file.
+    """
 
+    def __init__(self, id, fifo_queue: Queue, *, video_path,  timeout=0.1, fps=30):
+        """
+        id: The ID of the camera which will appear/or appear into the database.
+        video_path: The path to the video file to read frames from.
+        """
+        super().__init__(id, timeout=timeout, fifo_queue=fifo_queue, fps=fps)
+        self.video_path = video_path
+
+    def create_stream(self):
+        self.stream = VideoCapture(self.video_path)
